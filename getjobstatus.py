8cc7de4681abc5b506e4771b121b15ebf064d6d0
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-29 15:44:51 +0530

    Update getjobstatus.py

--- getjobstatus.py
+++ getjobstatus.py
@@ -1,7 +1,6 @@
-print("Hello")
-#import os
-#print(os.environ['JOB_NAME'])
-#print(os.environ['BUILD_NUMBER'])
-#print(os.environ['BUILD_URL'])
-#print(os.environ['BUILD_TAG'])
+import os
+print(os.environ['JOB_NAME'])
+print(os.environ['BUILD_NUMBER'])
+print(os.environ['BUILD_URL'])
+print(os.environ['BUILD_TAG'])
 
