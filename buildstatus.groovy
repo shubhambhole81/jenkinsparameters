ad8b39cb8123a6e91ffa90ec3f05ff1dec3b3fb8
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-29 16:17:32 +0530

    Update buildstatus.groovy

--- buildstatus.groovy
+++ buildstatus.groovy
@@ -1,4 +1,6 @@
 import org.jenkinsci.plugins.workflow.actions.TimingAction
+import io.jenkins.blueocean.rest.impl.pipeline.PipelineNodeGraphVisitor
+import io.jenkins.blueocean.rest.impl.pipeline.FlowNodeWrapper
 void printFinishedStageDurations() {
                     def visitor = new PipelineNodeGraphVisitor( currentBuild.rawBuild )
                     def stages = visitor.pipelineNodes.findAll{ it.type == FlowNodeWrapper.NodeType.STAGE }
