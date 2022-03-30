090edd0c4f71117a9b79406a269ffbcb316b0ca0
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-30 10:12:21 +0530

    Update getjobstatus.py

--- getjobstatus.py
+++ getjobstatus.py
@@ -1,6 +1,13 @@
-import os
-print("JOB_NAME: ",os.environ['JOB_NAME'])
-print("JOB-BUILD_NUMBER: ",os.environ['BUILD_NUMBER'])
-print("JOB-BUILD_URL: ",os.environ['BUILD_URL'])
-print("JOB-BUILD_TAG: ",os.environ['BUILD_TAG'])
+#import os
+#print("JOB_NAME: ",os.environ['JOB_NAME'])
+#print("JOB-BUILD_NUMBER: ",os.environ['BUILD_NUMBER'])
+#print("JOB-BUILD_URL: ",os.environ['BUILD_URL'])
+#print("JOB-BUILD_TAG: ",os.environ['BUILD_TAG'])
+import requests
 
+url = "https://http://localhost:8080/job/TestPipeline//lastBuild/wfapi/"
+response = requests.request("GET", url)
+data = response.json()
+print (data['stages'])
+print (data['id'])
+print (data['status'])
