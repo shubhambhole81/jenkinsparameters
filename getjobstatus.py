f6e8d441f8028eb72c9fea003b1f13609f3319ac
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-04-01 14:26:43 +0530

    Update getjobstatus.py

--- getjobstatus.py
+++ getjobstatus.py
@@ -3,10 +3,7 @@
 #print("JOB-BUILD_NUMBER: ",os.environ['BUILD_NUMBER'])
 #print("JOB-BUILD_URL: ",os.environ['BUILD_URL'])
 #print("JOB-BUILD_TAG: ",os.environ['BUILD_TAG'])
-import requests
-url = "https://localhost:8080/job/TestPipeline/"
-response = requests.request("GET", url,auth=('dharmendra','Kumar@123'))
-data = response.json()
-print (data['stages'])
-print (data['id'])
-print (data['status'])
+import time
+print("Hello World....!")
+time.sleep(10)
+
