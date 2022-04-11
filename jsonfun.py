1cffe0044e8835deaedd398b4abed270be58c189
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-04-11 09:56:14 +0530

    Update jsonfun.py

--- jsonfun.py
+++ jsonfun.py
@@ -16,7 +16,8 @@ try:
         build_status=data['status']
         pipeline_startime=datetime.fromtimestamp(data['startTimeMillis']//1000).isoformat()
         pipeline_endtime=datetime.fromtimestamp(data['endTimeMillis']//1000).isoformat()
-        pipeline_duration=(data['durationMillis']//1000)//60
+        pipeline_duration=data['durationMillis']
+        //pipeline_duration=(data['durationMillis']//1000)//60
         
         build_pipeline_data= { "Build_number": build_no,"build_status":build_status,"pipeline_startime":pipeline_startime," pipeline_endtime": pipeline_endtime,
                                  "pipeline_duration":pipeline_duration
@@ -27,7 +28,7 @@ try:
     def stage_info():
         stage_data = []
     # Extarcting the data from json and saving it to dictonary.    
-        for key in data['stages']:
+        for key in data['stages'][1:]:
             StageName = key['name']
             EndTime1 = key['startTimeMillis']+ key['durationMillis']
             EndTime= datetime.fromtimestamp(EndTime1//1000).isoformat()
